7ca1414923f9bea11d23941d9e95e04e045c4edc
DThaiPome <r.gehrlein154@gmail.com>
2020-10-01 15:13:15 -0400

    Add button not working

--- resources/scripts/leaderboard.js
+++ resources/scripts/leaderboard.js
@@ -1,5 +1,12 @@
 function add_player() {
     var name = document.new_player_info.player_name.value;
     var id = document.new_player_info.player_ripper_id.value;
-    alert(name + " " + id);
+    var newRow = document.rankings_table.createElement("tr");
+    var nameCol = newRow.createElement("th");
+    var idCol = newRow.createElement("th");
+    var eloCol = newRow.createElement("th");
+
+    nameCol.createTextNode(name);
+    idCol.createTextNode(id);
+    eloCol.createTextNode(1000);
 }
