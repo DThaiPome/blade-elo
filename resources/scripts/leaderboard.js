88e25cae9e68cae247f36c70dc2464e68bc2ebf9
DThaiPome <r.gehrlein154@gmail.com>
2020-10-01 16:04:05 -0400

    Reformatted table code - now it makes changes to the array, then renders

--- resources/scripts/leaderboard.js
+++ resources/scripts/leaderboard.js
@@ -8,33 +8,59 @@ class Player {
 
 var leaderboard = [];
 
+function get_table_id() {
+    return "rankings";
+}
+
 function add_player() {
-    var rankings_table = document.getElementById("rankings");
+    var rankings_table = document.getElementById(get_table_id());
 
     var name = document.new_player_info.player_name.value;
     var id = document.new_player_info.player_ripper_id.value;
 
-    var rowCount = rankings_table.rows.length;
-    var newRow = rankings_table.insertRow(rowCount);
-    newRow.id = "player" + rowCount;
-
-    var idCol = newRow.insertCell(0);
-    idCol.class = "RipperID";
-
-    var nameCol = newRow.insertCell(1);
-    nameCol.class = "Identity";
-
-    var eloCol = newRow.insertCell(2);
-    eloCol.class = "ELO";
-
     var initialElo = 1000;
 
-    nameCol.innerHTML = name;
-    idCol.innerHTML = id;
-    eloCol.innerHTML = initialElo;
-
-    var newPlayer = new Player(name, id, initialElo);
+    var newPlayer = new Player(name, id, 1000);
     leaderboard.push(newPlayer);
 
-    console.log(JSON.stringify(leaderboard));
+    render_table();
+}
+
+function clear_players() {
+    leaderboard = [];
+    render_table();
+}
+
+function render_table() {
+    clear_table(get_table_id());
+    add_rows_from_leaderboard(get_table_id());
+}
+
+function clear_table(tableID) {
+    var rankings_table = document.getElementById(tableID);
+
+    while(rankings_table.rows.length > 1) {
+        rankings_table.rows[1].remove();
+    }
+}
+
+function add_rows_from_leaderboard(tableID) {
+    var rankings_table = document.getElementById(tableID);
+    leaderboard.forEach( (item, index) => {
+        var newRow = rankings_table.insertRow(index + 1);
+        newRow.id = "player" + index;
+
+        var idCol = newRow.insertCell(0);
+        idCol.class = "RipperID";
+
+        var nameCol = newRow.insertCell(1);
+        nameCol.class = "Identity";
+
+        var eloCol = newRow.insertCell(2);
+        eloCol.class = "ELO";
+        
+        nameCol.innerHTML = item.name;
+        idCol.innerHTML = item.ripper_id;
+        eloCol.innerHTML = item.elo;
+    })
 }
