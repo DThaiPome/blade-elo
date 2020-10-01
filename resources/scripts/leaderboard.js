d4c23ab06201f8e7dd8d09159bbd2652abcced55
DThaiPome <r.gehrlein154@gmail.com>
2020-10-01 15:27:58 -0400

    Adding rows works

--- resources/scripts/leaderboard.js
+++ resources/scripts/leaderboard.js
@@ -1,12 +1,23 @@
 function add_player() {
+    var rankings_table = document.getElementById("rankings");
+
     var name = document.new_player_info.player_name.value;
     var id = document.new_player_info.player_ripper_id.value;
-    var newRow = document.rankings_table.createElement("tr");
-    var nameCol = newRow.createElement("th");
-    var idCol = newRow.createElement("th");
-    var eloCol = newRow.createElement("th");
 
-    nameCol.createTextNode(name);
-    idCol.createTextNode(id);
-    eloCol.createTextNode(1000);
+    var rowCount = rankings_table.rows.length;
+    var newRow = rankings_table.insertRow(rowCount);
+    newRow.id = "player" + rowCount;
+
+    var idCol = newRow.insertCell(0);
+    idCol.class = "RipperID";
+
+    var nameCol = newRow.insertCell(1);
+    nameCol.class = "Identity";
+
+    var eloCol = newRow.insertCell(2);
+    eloCol.class = "ELO";
+
+    nameCol.innerHTML = name;
+    idCol.innerHTML = id;
+    eloCol.innerHTML = 1000;
 }
