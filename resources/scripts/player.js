75664690bf6a7caa6d75f71d614229849a074162
Lmgund <55705814+Lmgund@users.noreply.github.com>
2020-10-02 21:48:43 -0400

    Update player.js

--- resources/scripts/player.js
+++ resources/scripts/player.js
@@ -15,13 +15,36 @@ class player {
         this.tourneywins = tourneywins;
     }
     constructor(name,ripper_id){ //creates a new beybaby
-      this(name,ripper_id,800,0,0,0,0,0,0,0,0,0,0,0);
+      this(name,ripper_id,1000,0,0,0,0,0,0,0,0,0,0,0);
     }
+
+    get_elo(){
+        return this.elo;
+    }
+
+    //calculates win percentage based on the total number of matches (wins and loses)
     get_win_percent(){
         return (this.wins / (this.wins + this.loses))*100;
     }
 
+    //calculates the outspin percentage based on the total number of outspins and outspuns
     get_outspin_percent(){
         return (this.outspins / (this.outspins + this.outspuns)) * 100;
+    }
+
+    //assumes this player is the winner and updates the elo of both this player and their opponent
+    Win_adjust_elo(opp){
+        var k = scaleK(this.elo);
+
+        var expected = 1 / (1 + Math.pow(10, ((opp.elo - this.elo)/400)));
+        var this_adjustedElo = Math.max(this.elo + Math.ceil(k * (1 - expected)), 800);
+        var opp_adjustedElo = Math.max(opp.elo +_Math.cel(k* ( 1 - (1-expected),800)));
+        this.setElo(this_adjustedElo);
+        opp.setElo(opp_adjustedElo);
+    }
+
+    //sets the elo if this player to the given number
+    setElo(elo){
+        this.elo = elo;
     }
 }
